5878b111a9350e5d285d91d75efb67f4b642e948
hoang <huuhoang.nguyen@servicenow.com>
2025-08-16 22:09:12 +0000

    Adding documentation

--- utils/constants.py
+++ utils/constants.py
@@ -107,7 +107,7 @@ mtbench_temp_map = {
 
 allowed_task_metrics = {
     # ASR
-    'generic_asr': ['word_error_rate', 'meteor', 'bleu', 'bertscore'],
+    'general_asr': ['word_error_rate', 'meteor', 'bleu', 'bertscore'],
     'code_switching_asr': ['word_error_rate', 'meteor', 'bleu', 'bertscore'],
     'long_form_asr': ['word_error_rate', 'meteor', 'bleu', 'bertscore'],
     'mls': ['word_error_rate', 'meteor', 'bleu', 'bertscore'],
